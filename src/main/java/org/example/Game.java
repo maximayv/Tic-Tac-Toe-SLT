1b8770f227ed9b64909cdb2e30ae78bfcdcf1f21
maximayv <w24b182@technikum.wien.at>
2025-07-06 15:18:53 +0100

    User Story 003: Spielende erkennen

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -28,5 +28,29 @@ public class Game {
             System.out.println();
         }
     }
+    public char checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+                return board[i][0];
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
+                return board[0][i];
+        }
+        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+            return board[0][0];
+        if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+            return board[0][2];
+
+        return ' '; // Kein Gewinner
+    }
+    public boolean isFull() {
+        for (char[] row : board) {
+            for (char cell : row) {
+                if (cell == ' ') return false;
+            }
+        }
+        return true;
+    }
+
+
 
 }
